20e8d9af028cb207767f87ac7cd20b5662456e2b
klozz <carlosj@klozz.dev>
2025-03-10 16:23:15 -0600

    TouchScreenGesturesSettings: adapt to xperience
    Signed-off-by: klozz <carlosj@klozz.dev>

--- src/com/android/touch/gestures/TouchscreenGestureSettings.java
+++ src/com/android/touch/gestures/TouchscreenGestureSettings.java
@@ -31,8 +31,8 @@ import androidx.preference.Preference;
 import androidx.preference.PreferenceFragment;
 import androidx.preference.PreferenceManager;
 
-import com.android.internal.yaap.hardware.LineageHardwareManager;
-import com.android.internal.yaap.hardware.TouchscreenGesture;
+import com.android.internal.xpe.hardware.LineageHardwareManager;
+import com.android.internal.xpe.hardware.TouchscreenGesture;
 
 import com.android.touch.gestures.R;
 import com.android.touch.gestures.utils.ResourceUtils;
